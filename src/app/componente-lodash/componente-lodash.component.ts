fb03fa1e02510d8bb0b949beb459f5995f1a449d
jesubu <fjdiazr@gmail.com>
2017-12-27 07:59:11 +0100

    error

--- src/app/componente-lodash/componente-lodash.component.ts
+++ src/app/componente-lodash/componente-lodash.component.ts
@@ -64,7 +64,7 @@ export class ComponenteLodashComponent implements OnInit {
 
     //Clonamos un objeto
 var objB = _.cloneDeep(datos);
-console.log('Clonamos un objeto':objB);
+console.log('Clonamos un objeto'+objB);
 
 
   }
